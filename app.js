9767862011fa733237a46d58e7870e329922229a
Rhonda Lawrence <rhonda.lawrence@verizon.com>
2019-07-30 09:36:48 -0500

    Proj6 photos added and coding

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 const express = require('express');
+
 const data = require('./data.json');
+
 const app = express();
+
 const path = require('path');
+
 app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.static(path.join(__dirname, '/images/')));
+
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.use('/images', express.static(path.join(__dirname, '/images/')));
-app.use('/static', express.static('public'));
 
 app.get('/', function (request, response) {
-    console.log(data);
-response.render('index');
+    
+    response.render('index', { projects: data.projects});    
 });
+app.get('about', function (request, response) {
+    
+    response.render('about', { projects: data.projects});    
+});
+
+app.get('/index', function (request, response) {
+    
+    response.render('index', { projects: data.projects });
+});
+
+app.get('/project', function (request, response) {
+
+    let projectIndex = request.query.id;
+
+    response.render('project', { project: data.projects[projectIndex] });
+});
+
 app.listen('3000', () => {
-console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
+
