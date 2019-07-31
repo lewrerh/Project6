7b26f46392e4cdc925bd22ef358602feb78fe9d9
Rhonda Lawrence <rhonda.lawrence@verizon.com>
2019-07-31 07:44:18 -0500

    addl notes added completing file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project6
- Static Node
+ Static Node.js and Express Site
+ This project is composed of my first five projects in the Treehouse tech degree program. It displays examples of the various projects using JSON, Node.js and Express working on the server-side of JavaScript. This is a portfolio of my work, with a little details about myself.
+ I have demonstrated setting up routes to handle request, middleware to utilize static files
+ like CSS, handle errors and set up a server to serve this project. This project also includes
+ the exceeds features creating a Pug template in the view folder, extending the layout, and displaying the error.message. I've added to the background color, box shadows and transitions for extra credit as well.
--- app.js
+++ app.js
@@ -1,42 +1,60 @@
-const express = require('express');
+const express = require('express');   // Create the express object
 
-const data = require('./data.json');
+const data = require('./data.json');  // Get the JSON project data 
 
-const app = express();
+const app = express();                // Create the app object
 
-const path = require('path');
+const path = require('path');         // Create the path object
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug');        // Set the app view engine to pug
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));   // Set the static folder for the public folder
 
-app.use(express.static(path.join(__dirname, '/images/')));
+app.use(express.static(path.join(__dirname, '/images/')));  // Set the static folder for the images folder
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));  // Set the static folder for the public folder
 
-app.use('/images', express.static(path.join(__dirname, '/images/')));
+app.use('/images', express.static(path.join(__dirname, '/images/')));  // Set the static folder for the images folder
 
-app.get('/', function (request, response) {
+app.get('/', function (request, response) {                // Set an app route for localhost
     
-    response.render('index', { projects: data.projects});    
+    response.render('index', { projects: data.projects});   // Set an app route for the index 
 });
-app.get('/about', function (request, response) {
+app.get('/about', function (request, response) {           // Set an app route for the about
     
     response.render('about');    
 });
 
-app.get('/index', function (request, response) {
+app.get('/index', function (request, response) {          // Set an app route for the index
     
     response.render('index', { projects: data.projects });
 });
 
-app.get('/project', function (request, response) {
+app.get('/project', function (request, response) {        // Set an app route for the project
 
     let projectIndex = request.query.id;
 
     response.render('project', { project: data.projects[projectIndex] });
 });
 
+// Set an error handling middleware for the project
+app.use(function (request, response, next) {
+
+    const err = new Error('Server error: Your requested page does not exist');
+    err.status = 404;
+    next(err);
+});
+
+// Handle errors
+app.use(function (err, request, response, next) {
+
+    response.locals.error = err;
+    response.render('error', { error: err });
+    console.log("Server error: Your requested page does not exist");
+
+});
+
+// Start server listening on port 3000
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
